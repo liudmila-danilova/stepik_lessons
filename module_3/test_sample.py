2d48f9767385bebd230af1c3eb11e532cf9527fe
Liudmila Danilova <liudmila.danilova@playgendary.com>
2021-02-17 00:27:48 +0300

    homework#3

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import math
+
 
 try:
     #Тестовый сценарий 1.1.1 Просмотр главной страницы при незалогиненном пользователе
@@ -19,25 +18,22 @@ try:
     #проверяем на видимость блок выбора языка
     lang = WebDriverWait(browser, 20).until(
         EC.visibility_of_element_located((By.ID, "language_selector")))
-    print(lang.get_attribute("id"))
 
+    #проверка на видимость блока логина
     login_btn = WebDriverWait(browser, 20).until(
         EC.visibility_of_element_located((By.ID, "login_link")))
-    #actual_url_login = login_btn.get_attribute("href")
+    actual_url_login = login_btn.get_attribute("href")
 
     #сравнение актуальной ссылки на логин с ожидаемой
-    #assert "http://selenium1py.pythonanywhere.com/ru/accounts/login/" == actual_url_login
-    #print(lang_btn.get_attribute("href"))
-
-    #basket = WebDriverWait(browser, 20).until(
-        #EC.visibility_of_element_located((By.CLASS_NAME, "basket-mini pull-right hidden-xs")))
-    #actual_url_login = login_btn.get_attribute("href")
-
-    # сравнение актуальной ссылки на логин с ожидаемой
-    #assert "http://selenium1py.pythonanywhere.com/ru/accounts/login/" == actual_url_login
-    #print(basket.get_attribute("class"))
+    assert "http://selenium1py.pythonanywhere.com/ru/accounts/login/" == actual_url_login
 
 
+    basket_look = WebDriverWait(browser, 20).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "span>a")))
+    actual_url_basket_look = basket_look.get_attribute("href")
+
+    #сравнение актуальной ссылки на корзину с ожидаемой
+    assert "http://selenium1py.pythonanywhere.com/ru/basket/" == actual_url_basket_look
 
 finally:
     browser.quit()
